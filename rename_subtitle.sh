724031590cddfff5a028182c8487fcfb49394de1
Sulaiman <xSloMxSA@gmail.com>
2023-07-22 01:22:06 +0300

    Add arguments, echo_then_run and check_input functions.
    - The arguments we have is language the user specify and the folder path that he want to check for subtitles.
    - echo_then_run function will echo the command and then run it.
    - check_input will check for the user input if it's valid or not.

--- rename_subtitle.sh
+++ rename_subtitle.sh
@@ -7,3 +7,35 @@ function usage() {
     echo "e.g. $0 ar /Movies/MyMovie"
     exit 1
 }
+
+# Check that the user has provided the correct number of arguments
+if [ $# -lt 2 ]; then
+    usage
+fi
+
+LANGUAGE=$1    # e.g. ar
+FOLDER_PATH=$2 # e.g. /Movies/MyMovie
+
+# Echo the command and then run it
+function echo_then_run() {
+    echo "$@"
+    "$@"
+}
+
+# Check that the user has provided the correct arguments
+function check_input() {
+    if [[ -z "$FOLDER_PATH" ]]; then
+        echo "Please provide a folder path as an argument"
+        exit 1
+    fi
+
+    if [[ ! -d "$FOLDER_PATH" ]]; then
+        echo "Folder does not exist"
+        exit 1
+    fi
+
+    if [[ -z "$LANGUAGE" ]]; then
+        echo "Please provide a language code as an argument"
+        exit 1
+    fi
+}
